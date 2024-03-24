d0c370e818d054e17a8a198c7ee1b6f1e42c64e0
Nare Ohanyan <nar.ohanyan10@gmaile.com>
2024-03-24 22:16:58 +0400

    added classes city, street, crossroad

--- City.h
+++ City.h
@@ -5,4 +5,48 @@
 #ifndef BUSES_1_CITY_H
 #define BUSES_1_CITY_H
 
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
+
+class City {
+private:
+    int numberOfCrossroads;
+    int numberOfStreets;
+    std::vector<std::vector<int>> city;
+    std::unordered_map<Crossroad, std::unordered_set<Street>> crossroadToStreets;
+    std::vector<int> buses;
+
+public:
+    City(int _numberOfCrossroads, int _numberOfStreets, std::vector<Street> _streets) : numberOfCrossroads(_numberOfCrossroads), numberOfStreets(_numberOfStreets), city(std::vector<int>(N, std::vector<int>(N, INT_MAX))), buses(std::vector<int>) {
+        for(int i = 0; i < numberOfCrossroads; ++i) {
+            for(int j = 0; j < numberOfCrossroads; ++j) {
+                if(i == j) {
+                    city[i][j] == 0;
+                }
+            }
+        }
+
+        for(int k = 0; k < numberOfStreets; ++k) {
+                char firstCrossroadValue = _streets[k].getFirst().getValue();
+                char secondCrossroadValue = _streets[k].getSecond().getValue();
+
+                int i = firstCrossroadValue - 65;
+                int j = secondCrossroadValue - 65;
+
+                city[i][j] = _streets[k].getDuration();
+        }
+
+        for(int i = 0; i < numberOfCrossroads; ++i) {
+            char key = (char) (i + 65);
+
+            for(int j = 0; j < numberOfStreets; ++j) {
+                if(_streets[j].getFirst() == key || _streets[j].getSecond() == key) {
+                    crossroadToStreets[key].insert(_streets[j]);
+                }
+            }
+        }
+    }
+};
 #endif //BUSES_1_CITY_H
--- Crossroad.h
+++ Crossroad.h
@@ -5,4 +5,20 @@
 #ifndef BUSES_1_CROSSROAD_H
 #define BUSES_1_CROSSROAD_H
 
+//#include <functional>
+
+class Crossroad {
+private:
+    const char value;
+
+public:
+    Crossroad(const char &_value) : value(_value) {}
+
+    char getValue() const {
+        return value;
+    }
+
+    ~Crossroad() {}
+};
+
 #endif //BUSES_1_CROSSROAD_H
