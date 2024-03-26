3ef337b5c4d2d56b8716035b1c2a2f1d765beb07
Nare Ohanyan <nar.ohanyan10@gmaile.com>
2024-03-26 14:07:51 +0400

    added classes city, street, crossroad

--- Crossroad.h
+++ Crossroad.h
@@ -1,7 +1,3 @@
-//
-// Created by Nare on 3/23/2024.
-//
-
 #ifndef BUSES_1_CROSSROAD_H
 #define BUSES_1_CROSSROAD_H
 
--- Street.h
+++ Street.h
@@ -1,7 +1,3 @@
-//
-// Created by Nare on 3/23/2024.
-//
-
 #ifndef BUSES_1_STREET_H
 #define BUSES_1_STREET_H
 
